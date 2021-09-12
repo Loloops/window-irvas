4b93b0979bf625e0a82b4c5b6a024d06b490f0b5
V <sss.5151@mail.ru>
2021-09-12 18:01:51 +0400

    Небольшие правки теперь таймер у модалки ремувается при клике на любой триггер модального окна, так же небольшая анимация у табов(говно)

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,8 @@ const modals = () => {
     const trigger = document.querySelectorAll(triggerSelector),
           modal = document.querySelector(modalSelector),
           close = document.querySelector(closeSelector),
-          windows = document.querySelectorAll('[data-modal]');
+          windows = document.querySelectorAll('[data-modal]'),
+          scroll = calcScroll();
 
 
     trigger.forEach(item => {
@@ -18,7 +19,7 @@ const modals = () => {
 
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
-        // document.body.classList.add('modal-open');//Возможности стилей boosttrap
+        document.body.style.marginRight = `${scroll}px`;
       });
     });
 
@@ -28,7 +29,7 @@ const modals = () => {
       });  
       modal.style.display = 'none';
       document.body.style.overflow = '';
-      // document.body.classList.remove('modal-open');//Возможности стилей boosttrap
+      document.body.style.marginRight = `0px`;
     });
 
     modal.addEventListener('click', (e) => {
@@ -39,15 +40,39 @@ const modals = () => {
         modal.style.display = 'none';
         document.body.style.overflow = '';
         // document.body.classList.remove('modal-open');//Возможности стилей boosttrap
+        document.body.style.marginRight = `0px`;
       }
     });
   }
 
-  function showModalByTime(selector, time) {
-    setTimeout(function () {  
+  function showModalByTime(selector, time, ...triggerd) {
+    let trigger = document.querySelectorAll(triggerd);
+   let b = setTimeout(function () {  
       document.querySelector(selector).style.display = 'block';
       document.body.style.overflow = 'hidden';
     }, time);
+    trigger.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if (e.target) { //проверка на существование элемента
+          e.preventDefault();
+        }
+        clearTimeout(b)
+      });
+    });
+  }
+
+  function calcScroll() {
+    let div = document.createElement('div');
+    div.style.width = '50px'
+    div.style.height = '50px'
+    div.style.overflowY ='scroll'
+    div.style.visibility = 'hidden'
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove()
+
+    return scrollWidth
   }
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
@@ -55,6 +80,6 @@ const modals = () => {
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
   bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
   bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
-  showModalByTime('.popup', 60000);
+  showModalByTime('.popup', 60000, '.popup_calc_btn', '.popup_engineer_btn', '.phone_link');
 };
 export default modals;
